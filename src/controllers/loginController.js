c0686e8f0c587c42f25521301961af8683e98346
Gabrielle Gonçalves <118405222+gooncalvesgaby@users.noreply.github.com>
2023-05-31 21:21:52 -0300

    Update loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -6,33 +6,30 @@ const carrinhoProdutos = require('../database/carrinhoProdutos.json');
 const {User} = require('../models')
 
 const loginController = {
-// aqui é o meu atributo função   
-    renderizarTelaLogin: (req, res) => {
-        res.render('login')
-    },
+
     cadastrarUsuario: async (req, res) => {
         //aqui eu fiz uma receitinha de bolo pro meu sistema me retornar caso encontre erros
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             console.log(errors.mapped())
-            res.render('login', { errors: errors.array() })
+            res.send( { errors: errors.array() })
         } else {
             const data = req.body
-            console.log(data)
+           
             const usuario = await User.findOne({ where: { email: data.email } })
             if (usuario) {
-                res.render('login', {errors:[{msg: 'Email já em uso.'}]})
+                res.send({msg: 'Email já em uso.'})
             }
 
             else if (data.senha !== data.confirmarsenha ){
-                res.render('login', {errors: [{ msg: 'Senhas não coincidem.' }]})
+                res.send( {errors: [{ msg: 'Senhas não coincidem.' }]})
             }
 
             data.administrador = 0
             delete data.confirmarsenha
 
             const newUser = await User.create(data);
-            res.render('login', {sucess: [{msg: 'Usuario ' + newUser.nome + ' criado com sucesso'}]})
+            res.send({sucess: [{msg: 'Usuario ' + newUser.nome + ' criado com sucesso'}]})
 
            
         }
@@ -43,7 +40,7 @@ const loginController = {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             console.log(errors.mapped())
-            res.render('login', { errors: errors.array() })
+            res.send({ errors: errors.array() })
         } else {
 
             //req.body é onde o front me envia infos, aqui eu recebo email e senha enviado pelo usuario
@@ -54,7 +51,7 @@ const loginController = {
             console.log('>>>>>',usuario)
             // se caso o usuario nao for cadastrado 
             if(usuario === null) {
-                res.render('login', { errors: [{ msg:'Usuario não encontrado' }] })
+                res.send( { errors: [{ msg:'Usuario não encontrado' }] })
             } else {
                 const userLogged = usuario.dataValues
                 let iniciais = ''
@@ -62,16 +59,12 @@ const loginController = {
                 iniciais += userLogged.sobrenome.substring(0, 1)
                 userLogged.iniciais = iniciais
                 console.log('usuario>>>', usuario.dataValues);
-                res.render('home', { usuario: userLogged, carrinhoProdutos: carrinhoProdutos  });
+                res.send({ userLogged });
 
             }
         }
 
         
-    },   
-    deslogarUsuario: (req, res) => {
-        res.render('home', { allProducts, carrinhoProdutos: carrinhoProdutos  });
-
     }
 } 
 // aqui eu to exportando o meu objeto
