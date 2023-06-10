279b01eef593e190e961307945f919d7e528063f
Matheus Lopes <matheuuslopes13@gmail.com>
2023-06-09 22:02:58 -0300

    resolvendo problema do CORS

--- src/app.js
+++ src/app.js
@@ -1,15 +1,17 @@
 const routes = require('./routes/index')
 const path = require("path")
 const express = require('express')
+const cors = require('cors');
 const app = express();
 const methodOverride = require('method-override');
-
 
 app.use(express.json())
 app.use(methodOverride('_method'));
 app.use(express.urlencoded({ extended: false }))
 app.use(express.static(path.resolve("public")))
+app.use(cors())
 app.use(routes)
+
 
 app.listen(3000, () => {
   console.log('Servidor Rodando!');
--- src/routes/index.js
+++ src/routes/index.js
@@ -12,9 +12,9 @@ const perfilController = require('../controllers/perfilController')
 
 
 // Rotas ja em padrão REST
-router.get('/',  homeController.home)
+router.get('/allProducts',  homeController.home)
 router.get('/detals/:id', detalsController.getProduct)
-router.delete('/admin/delete/:id', adminController.deleteProduct)
+router.delete('/admin/delete/', adminController.deleteProduct)
 router.put('/update/:id', adminController.updateProduct)
 router.post('/product',
 //     body('nome').notEmpty().withMessage('Marca precisa ser preenchido!'),
@@ -30,26 +30,26 @@ router.post('/product',
 
 
 
-router.get('/search', homeController.search)
-router.get('/tenis', teniscontroller.tenis)
-router.get('/admin', adminController.renderizarAdminPage)
-router.get('/login', loginController.renderizarTelaLogin) 
-router.get('/pagamento', pagamentoController.pagamentoPage)
-router.get('/roupas', roupasController.roupasPage)
-router.post('/logar', loginController.logarUsuario)
-router.get('/perfil', perfilController.renderizarTelaPerfil)
-router.post('/editar/perfil', perfilController.editarPerfil)
-router.post('/criarconta',
-body('nome').notEmpty().withMessage('Nome precisa ser preenchido!'),
-body('sobrenome').notEmpty().withMessage('Sobrenome precisa ser preenchido!'), 
-body('email').notEmpty().withMessage('Email precisa ser preenchido!'),
-body('endereço').notEmpty().withMessage('Rua precisa ser preenchido!'),
-body('numero').notEmpty().withMessage('Número precisa ser preenchido!'),
-//body('cep').notEmpty().withMessage('CEP precisa ser preenchido!'),
-body('senha').notEmpty().withMessage('Senha precisa ser preenchido!'),
-body('confirmarSenha').isEmpty().withMessage('Confirmar senha precisa ser preenchido!'),
-loginController.cadastrarUsuario)
-router.get('/sair', loginController.deslogarUsuario)
+// router.get('/search', homeController.search)
+// router.get('/tenis', teniscontroller.tenis)
+// router.get('/admin', adminController.renderizarAdminPage)
+
+// router.get('/pagamento', pagamentoController.pagamentoPage)
+// router.get('/roupas', roupasController.roupasPage)
+// router.post('/logar', loginController.logarUsuario)
+// router.get('/perfil', perfilController.renderizarTelaPerfil)
+// router.post('/editar/perfil', perfilController.editarPerfil)
+// router.post('/criarconta',
+// body('nome').notEmpty().withMessage('Nome precisa ser preenchido!'),
+// body('sobrenome').notEmpty().withMessage('Sobrenome precisa ser preenchido!'), 
+// body('email').notEmpty().withMessage('Email precisa ser preenchido!'),
+// body('endereço').notEmpty().withMessage('Rua precisa ser preenchido!'),
+// body('numero').notEmpty().withMessage('Número precisa ser preenchido!'),
+// //body('cep').notEmpty().withMessage('CEP precisa ser preenchido!'),
+// body('senha').notEmpty().withMessage('Senha precisa ser preenchido!'),
+// body('confirmarSenha').isEmpty().withMessage('Confirmar senha precisa ser preenchido!'),
+// loginController.cadastrarUsuario)
+
 
 // router.get('/edit/:id', adminController.updateEjs)
 
